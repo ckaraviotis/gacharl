285c795c27c978d7fb1da69833f78a3497fed659
Christian Karaviotis <christian.karaviotis@gmail.com>
2019-05-19 22:27:45 +0100

    Draw basic map

--- constants.py
+++ constants.py
@@ -12,8 +12,20 @@ COLOR_GREY = (100, 100, 100)
 
 COLOR_DEFAULT_BG = COLOR_GREY
 
-# Sprites?
+# Map vars
+MAP_WIDTH = 30
+MAP_HEIGHT = 30
+
+# Sprites!
 SPRITE_WIDTH = 16
 SPRITE_HEIGHT = 16
+
+# Sprite Sheets
 DOGS = pygame.image.load("data/img/Characters/Dog1.png")
-S_PLAYER = DOGS.subsurface((0,0,SPRITE_WIDTH, SPRITE_HEIGHT))
+FLOOR = pygame.image.load("data/img/Objects/Floor.png")
+WALL = pygame.image.load("data/img/Objects/Wall.png")
+
+# Individual sprites
+S_PLAYER = DOGS.subsurface((0, 0, SPRITE_WIDTH, SPRITE_HEIGHT))
+S_FLOOR = FLOOR.subsurface((1 * SPRITE_WIDTH, 4 * SPRITE_WIDTH, SPRITE_WIDTH, SPRITE_HEIGHT))
+S_WALL = WALL.subsurface((0 * SPRITE_WIDTH, 5 * SPRITE_WIDTH, SPRITE_WIDTH, SPRITE_HEIGHT))
--- game.py
+++ game.py
@@ -1,20 +1,49 @@
+'''
+Main Game
+'''
+
+import collections
 import pygame
 import libtcodpy as libtcod
 import constants
+from tile import Tile
 
-def draw_game():
+def create_map():
+    ''' Generate a map '''
+    new_map = [[Tile(True) for y in range(0, constants.MAP_HEIGHT)] for x in range(0, constants.MAP_WIDTH)]
+
+    new_map[10][10].passable = False
+    new_map[10][15].passable = False
+    
+    return new_map
+
+def render_level(level, surface):
+    ''' The map rendering method '''
+    for x in range(0, constants.MAP_WIDTH):
+        for y in range(0, constants.MAP_HEIGHT):
+            if level[x][y].passable:
+                # draw floor
+                surface.blit(constants.S_FLOOR, (x * constants.SPRITE_WIDTH, y * constants.SPRITE_HEIGHT))
+            else:
+                # draw wall
+                surface.blit(constants.S_WALL, (x * constants.SPRITE_WIDTH, y * constants.SPRITE_HEIGHT))
+
+
+
+def render(level, surface):
     ''' The main game rendering method '''
-    global SURFACE_MAIN
     # Clear the display
-    SURFACE_MAIN.fill(constants.COLOR_DEFAULT_BG)
+    surface.fill(constants.COLOR_DEFAULT_BG)
+
+    render_level(level, surface)
 
     # Draw character
-    SURFACE_MAIN.blit(constants.S_PLAYER, (200, 200))
+    surface.blit(constants.S_PLAYER, (200, 200))
 
     # Update the display
     pygame.display.flip()
 
-def game_main_loop():
+def game_main_loop(surface, level):
     ''' The main game loop '''
     game_quit = False
     while not game_quit:
@@ -26,7 +55,7 @@ def game_main_loop():
                 game_quit = True
         
         # Render!
-        draw_game()
+        render(level, surface)
 
     # Quit the game
     pygame.quit()
@@ -34,10 +63,13 @@ def game_main_loop():
 
 def game_init():
     ''' Initialize the main game window & pygame '''
-    global SURFACE_MAIN
+    Game = collections.namedtuple('Game', ['surface', 'level'])
     pygame.init()
-    SURFACE_MAIN = pygame.display.set_mode((constants.WINDOW_WIDTH, constants.WINDOW_HEIGHT))
+    surface = pygame.display.set_mode((constants.WINDOW_WIDTH, constants.WINDOW_HEIGHT))
+    level = create_map()
+
+    return Game(surface, level)
 
 if __name__ == '__main__':
-    game_init()
-    game_main_loop()
+    GAME = game_init()
+    game_main_loop(GAME.surface, GAME.level)
